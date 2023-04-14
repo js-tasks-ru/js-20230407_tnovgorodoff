87ef32770cfcad9d71940704a17ddc15f43db0c7
TimNovgorodov <tnovgorodoff@gmail.com>
2023-04-14 15:47:28 +0300

    ДЗ 02-javascript-data-types
    Решения задач 1-sort-strings, 2-pick, 3-omit

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,11 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
-
+export function sortStrings(arr, param = "asc") {
+  let sortOrders = {
+    asc: 1,
+    desc: -1,
+  };
+  return [...arr].sort((a, b) =>
+      sortOrders[param] * a.localeCompare(b, ["ru", "en"], { caseFirst: "upper" }));
 }
