26477f5aa6950b435d34ad67e2dd13d94056e511
TimNovgorodov <tnovgorodoff@gmail.com>
2023-04-21 18:06:41 +0300

    ДЗ 03-objects-arrays-intro-to-testing
    Решения задач 1-create-getter, 2-invert-object, 3-trim-symbols, 4-uniq

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,11 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (typeof obj !== 'object') return undefined;
 
+  const invertedObj = {};
+  for (const [key, val] of Object.entries(obj)) {
+    invertedObj[val] = key;
+  }
+  return invertedObj;
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,29 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === undefined) {
+    return string;
+  }
+  if (size === 0 || string.length === 0) {
+    return '';
+  }
+  let symArr = string.split(''),
+    num = 0,
+    prevSym;
+  const resArr = [];
 
+  for (let sym of symArr) {
+    if (prevSym === sym) {
+      if (num < size) {
+        num++;
+        resArr.push(sym);
+      }
+    }
+    else {
+        prevSym = sym;
+        num = 1;
+        resArr.push(sym);
+      }
+    }
+    return resArr.join('');
 }
