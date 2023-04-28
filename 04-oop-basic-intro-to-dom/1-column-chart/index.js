d239c6dd037503788b7c5acb4b7c89469fcc4dc6
TimNovgorodov <tnovgorodoff@gmail.com>
2023-04-28 19:46:02 +0300

    ДЗ 04 и частично 05
    задачи 1-column-chart и 1-notification

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,98 @@
 export default class ColumnChart {
+  constructor(
+    {
+      data = [],
+      label = '',
+      link = '',
+      value = 0,
+      formatHeading = (value) => value
+    } = {}
+  )
+  {
+    this.data = data;
+    this.label = label;
+    this.value = value;
+    this.link = link;
+    this.chartHeight = 50;
+    this.formatHeading = formatHeading;
 
+    this.render();
+  }
+
+  render = () => {
+    this.element = createHtmlElement(this.getTemplate({
+      data: this.getColumnProps(this.data),
+      label: this.label,
+      value: this.value,
+      link: this.link,
+      height: this.chartHeight,
+      formatHeading: this.formatHeading
+    }));
+
+    if (this.data.length === 0) this.element.classList.add("column-chart_loading");
+  }
+
+  initEventListeners = () => {}
+
+  update = () => {
+    this.render();
+  }
+
+  remove = () => {
+    this.element.remove();
+  }
+
+  destroy = () => {
+    this.remove();
+  }
+
+  getTemplate = ({ data, label, value, link, height, formatHeading } = {}) => {
+    return `
+      <div class="column-chart" style="--chart-height: ${height}">
+        <div class="column-chart__title">
+          Total ${label}
+          <a class="column-chart__link" href="${link}">View all</a>
+        </div>
+        <div class="column-chart__container">
+          <div data-element="header" class="column-chart__header">
+            ${formatHeading(value)}
+          </div>
+          <div data-element="body" class="column-chart__chart">
+            ${this.getColumnBody(data)}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  getColumnBody = (columnsProps) => {
+    let columns = ``;
+
+    for (let i = 0; i < columnsProps.length; i++) {
+      columns += `
+        <div style="--value: ${columnsProps[i].value}" data-tooltip="${columnsProps[i].percent}"></div>
+      `;
+    }
+    return columns;
+  }
+
+  getColumnProps = (data) => {
+    const maxValue = Math.max(...data);
+    const scale = 50 / maxValue;
+
+    return data.map(item => {
+      return {
+        percent: (item / maxValue * 100).toFixed(0) + '%',
+        value: String(Math.floor(item * scale))
+      };
+    });
+  }
 }
+
+const createHtmlElement = (html) => {
+  const div = document.createElement('div');
+  div.innerHTML = html;
+
+  return div.firstElementChild;
+}
+
